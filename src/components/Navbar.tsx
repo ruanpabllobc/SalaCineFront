2fa9c274e690cec088e107103018e4ff239ac086
ruanpabllobc <ruanpabllobc@gmail.com>
2025-07-18 02:23:58 -0300

    add: logo na navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,25 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <nav className="fixed top-0 left-0 right-0 h-[114px] bg-white border-b border-[#EBEBEB] flex items-center justify-center z-50">
+    <nav className="fixed top-0 left-0 right-0 h-[114px] bg-white border-b border-[#EBEBEB] flex items-center justify-between px-8 z-50">
+      <div className="flex items-center">
+        <Link href="/">
+          <Image
+            src="/salaCine.svg"
+            alt="Cinema Logo"
+            width={45}
+            height={66}
+            className="mr-4"
+          />
+        </Link>
+      </div>
+
       <div className="flex gap-[60px]">
         <Link
           href="/sessao"
@@ -28,6 +41,8 @@ export default function Navbar() {
           Sala
         </Link>
       </div>
+
+      <div className="w-[45px]"></div>
     </nav>
   );
 }
