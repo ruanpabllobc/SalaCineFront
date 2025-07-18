d26337897feea726ded8dcb0d8172c640022092c
ruanpabllobc <ruanpabllobc@gmail.com>
2025-07-18 02:35:22 -0300

    add: animação na tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,44 @@
-import FilmeForm from "@/components/FilmeForm";
-import SalaForm from "@/components/SalaForm";
-import SessaoForm from "@/components/SessaoForm";
+"use client";
 
-export default function Home() {
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function SplashScreen() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/filme");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen gap-10">
-      <FilmeForm />
-      <SalaForm />
-      <SessaoForm />
-    </main>
+    <>
+      <Head>
+        <title>SalaCine</title>
+      </Head>
+
+      <div
+        className="fixed inset-0 flex items-center justify-center bg-[#171717]"
+        style={{ zIndex: 9999 }}
+      >
+        <div className="flex items-center">
+          <Link href="/">
+            <Image
+              src="/salaCine-2.svg"
+              alt="Cinema Logo"
+              width={86}
+              height={134}
+              className="mr-4"
+            />
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
